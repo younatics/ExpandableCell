21a6248aef65be1117895288fadb3bd448b076f9
Seungyoun Yi <younatics@gmail.com>
2017-08-06 22:58:59 +0900

    ad xcode project

---
+++ ExpandableCell/ExpandableCell.h
@@ -0,0 +1,19 @@
+//
+//  ExpandableCell.h
+//  ExpandableCell
+//
+//  Created by YiSeungyoun on 2017. 8. 6..
+//  Copyright © 2017년 SeungyounYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExpandableCell.
+FOUNDATION_EXPORT double ExpandableCellVersionNumber;
+
+//! Project version string for ExpandableCell.
+FOUNDATION_EXPORT const unsigned char ExpandableCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExpandableCell/PublicHeader.h>
+
+
