d17b12b309003b1e4de5cd6726b89248676f182c
Seungyoun Yi <younatics@gmail.com>
2017-08-07 11:02:17 +0900

    Add delegate

---
+++ ExpandableCell/ExpandableCellDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ExpandableCellDelegate.swift
+//  ExpandableCell
+//
+//  Created by Seungyoun Yi on 2017. 8. 7..
+//  Copyright © 2017년 SeungyounYi. All rights reserved.
+//
+
+import Foundation
+
+protocol ExpandableCellDelegate {
+    
+}
